628c65605edb6f0de50a68bacdd7da940aa5e9dd
TeamPlayer99 <32104436+TeamPlayer99@users.noreply.github.com>
2023-03-04 02:29:18 +0200

    Completed singleton

---
+++ SingletonApp/SingletonApp.Lib/CashRegister.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonApp.Lib
+{
+    public sealed class CashRegister
+    {
+        private static int register = 0;
+        private static CashRegister instance;
+        public static CashRegister GetCashRegister()
+        {
+            if(instance == null)
+                instance = new CashRegister();
+            return instance;
+        }
+
+        public void Deposit(int amount)
+        {
+            register += amount;
+        }
+
+        public void Withdraw(int amout)
+        {
+            if(register >= amout)
+            {
+                register -= amout;
+            }
+            else { Console.WriteLine("Insufficent Credit"); }
+                
+        }
+
+        public int PrintRegister()
+        {
+            return register;
+        }
+    }
+}
---
+++ SingletonApp/SingletonApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SingletonApp.Lib;
+
+namespace SingletonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister a = new CashRegister();
+            CashRegister b = new CashRegister();
+            CashRegister c = new CashRegister();
+            CashRegister d = new CashRegister();
+
+            a.Deposit(100);
+            Console.WriteLine(a.PrintRegister());
+
+            b.Withdraw(20);
+            Console.WriteLine(b.PrintRegister());
+
+            c.Withdraw(30);
+            Console.WriteLine(c.PrintRegister());
+
+            d.Deposit(130);
+            Console.WriteLine(d.PrintRegister());
+        }
+    }
+}
